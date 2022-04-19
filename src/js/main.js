932434bc35056b84e98854aecbc8322852fbf442
Kolosov Arkady <arkolosov2020@yandex.ru>
2022-04-19 12:54:42 +0300

    completed parallax-matthew

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,5 @@ import * as flsFunctions from "./modules/webp.js";
 flsFunctions.isWebp();
 
 import "./libraries/jquery-3.6.0.min.js";
-import "./libraries/slick.min.js";
+import "./libraries/jquery.parallax.js";
+import "./modules/parallax.js"
---
+++ src/js/modules/parallax.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('.parallax-matthew__list>li').addClass('layer');
+  $('.parallax-matthew__list').parallax();
+  $('.wrapper-parallax-matthew').addClass('active');
+});
