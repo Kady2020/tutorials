960499c4475cd82392849129721ac1c492ce0671
Kolosov Arkady <arkolosov2020@yandex.ru>
2022-04-19 16:38:05 +0300

    completed checkbox

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,5 @@ flsFunctions.isWebp();
 import "./libraries/jquery-3.6.0.min.js";
 import "./libraries/jquery.parallax.js";
 import "./modules/parallax.js"
+import "./modules/checkbox.js"
+import "./modules/radio.js"
--- src/js/modules/radio.js
+++ src/js/modules/radio.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  $.each($('.radio__item'), function (index, val) {
+  $.each($('.radio-button'), function (index, val) {
     if ($(this).find('input').prop('checked') == true) {
       $(this).addClass('active');
     }
   });
-  $(document).on('click', '.radio__item', function (event) {
-    $(this).parents('.radio').find('.radio__item').removeClass('active');
-    $(this).parents('.radio').find('.radio__item input').prop('checked', false);
+  $(document).on('click', '.radio-button', function (event) {
+    $(this).parents('.radio').find('.radio-button').removeClass('active');
+    $(this).parents('.radio').find('.radio-button input').prop('checked', false);
     $(this).toggleClass('active');
     $(this).find('input').prop('checked', true);
     return false;
